c61d95a8f1543e8624547397faddcbb91799e50b
Mase Santos <hola@masesantos.work>
2020-08-04 14:33:03 -0500

    first commit

---
+++ src/components/UserPage/UserPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import LogOutButton from "../LogOutButton/LogOutButton";
+
+class UserPage extends Component {
+
+  // this component doesn't do much to start, just renders some user info to the DOM
+  render() {
+    return (
+      <div>
+        <h1 id="welcome">Welcome, {this.props.user.username}!</h1>
+        <p>Your ID is: {this.props.user.id}</p>
+        <LogOutButton className="log-in" />
+      </div>
+    );
+  }
+}
+
+// Instead of taking everything from state, we just want the user info.
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+// this allows us to use <App /> in index.js
+export default connect(mapStateToProps)(UserPage);
