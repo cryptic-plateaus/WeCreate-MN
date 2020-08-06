86ed45a37e78877cbfab6ae768e69a687e0ebbe2
Mase Santos <hola@masesantos.work>
2020-08-06 10:17:34 -0500

    Added route in App.js for 'SubmitNewOppPage'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -45,26 +45,18 @@ class App extends Component {
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
-              exact
-              path="/home"
-              component={UserPage}
-            />
+            <ProtectedRoute exact path="/home" component={UserPage} />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            />
+            <ProtectedRoute exact path="/info" component={InfoPage} />
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
-            
+            <Route path="/submit" component={SubmitNewOppPage} />
           </Switch>
           <Footer />
         </div>
       </Router>
-  )}
+    );}
 }
 
 export default connect()(App);
