d727150a990ff6883fc283960b5f2415bf2762c6
Mase Santos <hola@masesantos.work>
2020-08-05 23:36:05 -0500

    Adjusted 'LandingPage' image and title/subtitle

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import InfoCard from "../../components/EmployerServicesInfoCard/EmployerServicesInfoCard";
+
 
 class LandingPage extends Component {
 
     // this component doesn't do much to start, just renders some user info to the DOM
     render() {
         return (
-            <>
+          <>
             <div className="welcome">
-                <img className="image" src="images/6.png" />
-                <div className="message">
-                        <h1 className="title">WeCreate MN</h1>
-                        <h3 className="subtitle">A place for Minnesota-based creatives of 
-                        <br />color to find local, paid opportunities</h3>
-                </div>
+              <img className="image" src="images/6.png" />
+              <div className="message">
+                <h1 className="title">WeCreate MN</h1>
+                <h3 className="subtitle">
+                  A place for Minnesota-based creatives of
+                  <br />
+                  color to find local, paid opportunities
+                </h3>
+              </div>
             </div>
             <div className="services">
-            
+              <h3 className="subtitle">Services</h3>
+              <InfoCard />
             </div>
-            </>
+          </>
         );
     }
 }
